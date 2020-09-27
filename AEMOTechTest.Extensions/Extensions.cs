144ad1c7311b674d07c662bcea992ccb5d59959c
DESKTOP-7HDK1D0\Greg <classified.development@gmail.com>
2020-09-27 09:55:25 +0800

    Algorithm developed.

--- AEMOTechTest.Extensions/Extensions.cs
+++ AEMOTechTest.Extensions/Extensions.cs
@@ -12,7 +12,21 @@ namespace AEMOTechTest.Extensions
                 return new List<int>();
 
 
-            return new List<int>();
+            int index = 0, start = 0;
+            List<int> results = new List<int>();
+
+            while (index != -1)
+            {
+                index = corpus.IndexOf(term, start, StringComparison.InvariantCultureIgnoreCase);
+
+                if (index != -1)
+                {
+                    results.Add(index);
+                    start = index + term.Length;
+                }
+            }
+
+            return results;
         }
     }
 }
